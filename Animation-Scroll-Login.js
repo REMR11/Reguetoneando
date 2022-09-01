edabacb8a60f03f66436b27dd158149df814dfb8
Ronald Mejia <74267868+REMR11@users.noreply.github.com>
2022-09-01 02:42:37 -0600

    V7.7 Front SysCaly Provisional

---
+++ Animation-Scroll-Login.js
@@ -0,0 +1,130 @@
+window.sr = ScrollReveal().reveal("#Animation-Up", {
+    origin: "bottom",
+    distance: "100px",
+    duration: 1000,
+    opacity: 0.5,
+  });
+  window.sr = ScrollReveal().reveal("#Animation-Left", {
+    origin: "left",
+    distance: "100px",
+    duration: 2000,
+    opacity: 0,
+    delay:2300
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Right", {
+    origin: "right",
+    distance: "100px",
+    duration: 1000,
+    opacity: 0,
+    delay:2300
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show", {
+    origin: "bottom",
+    duration: 2000,
+    opacity: 0,
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show-D5", {
+    origin: "bottom",
+    duration: 3000,
+    opacity: 0,
+    escale:0,
+    delay: 1500
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show-D6", {
+    origin: "bottom",
+    duration: 2000,
+    opacity: 0,
+    delay: 1600
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show-D7", {
+    origin: "bottom",
+    duration: 2000,
+    opacity: 0,
+    delay: 1700
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show-D8", {
+    origin: "bottom",
+    duration: 2000,
+    opacity: 0,
+    delay: 1800
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show-D9", {
+    origin: "bottom",
+    duration: 2000,
+    opacity: 0,
+    delay: 1900
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show-D10", {
+    origin: "bottom",
+    duration: 2000,
+    opacity: 0,
+    delay: 2000
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show-D11", {
+    origin: "bottom",
+    duration: 2000,
+    opacity: 0,
+    delay: 2100
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show-D11-move", {
+    origin: "top",
+    distance:"100px",
+    duration: 2000,
+    opacity: 0,
+    delay: 2100
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Show", {
+    origin: "bottom",
+    duration: 2000,
+    opacity: 0,
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Top", {
+    origin: "top",
+    distance:"200px",
+    duration: 3000,
+    opacity: 0,
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Scale", {
+    origin: "top",
+    duration: 3000,
+    opacity: 0,
+    scale: 0,
+    delay:1800
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Rotate", {
+    origin: "top",
+    distance:"200px",
+    duration: 3000,
+    opacity: 0,
+    rotate: {
+        x: 15,
+        y: 5,
+        z: 30
+    }
+  });
+
+  window.sr = ScrollReveal().reveal("#Animation-Rotate-Reverse", {
+    origin: "top",
+    distance:"200px",
+    duration: 3000,
+    opacity: 0,
+    rotate: {
+        x: 15,
+        y: 5,
+        z: -30
+    }
+  });
---
+++ AnimationsAnime.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal().reveal("#Chart-Number-Teachers", {
+    origin: "left",
+    distance: "100px",
+    duration: 1000,
+    opacity: 0,
+    scale: 0.85
+  });
+
+
